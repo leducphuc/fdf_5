584704fd9cae924aeaff0cddfb1f75cd417c6290
Luu Kha <luu.tuan.kha@framgia.com>
2016-07-04 10:49:28 +0700

    init model final

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
+  has_many :ratings, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :product_orders, dependent: :destroy
+  
   belongs_to :category
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :suggests, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :ratings, dependent: :destroy
+  has_many :orders, dependent: :destroy
 end
