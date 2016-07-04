7520c4b9d433f8a3407b1bed91a561f1d32eef5f
Luu Kha <luu.tuan.kha@framgia.com>
2016-07-04 10:49:28 +0700

    init model final
    Generate Static Pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title title = ""
+    base_title = t "application.base_title"
+    if title.blank?
+      base_title
+    else
+      title << "|" << base_title
+    end
+  end
 end
