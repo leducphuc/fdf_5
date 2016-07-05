cf38e79f3960b275e445d1407391e828d4fa3e79
sonvc94 <sonvc94@hotmail.com>
2016-07-05 14:34:54 +0700

    delete-user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,26 @@
-User.create!
+User.create(
   name: "admin",
   email: "admin@admin.com",
   password: "123456", 
   password_confirmation: "123456",
   is_admin: true 
+)
+User.create(
+  name: "nobita",
+  email: "nobita@123.com",
+  password: "123456", 
+  password_confirmation: "123456",
+  is_admin: true
+  )
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create(
+    name:  name,
+    email: email,
+    password:              password,
+    password_confirmation: password,
+    is_admin: false
+  )
+end
