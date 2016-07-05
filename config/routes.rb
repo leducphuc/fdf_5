972da8d2c4efbe9019d8348b2ac15dacc8a17b93
sonvc94 <sonvc94@hotmail.com>
2016-07-05 08:07:24 +0700

    login-logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  
   root "static_pages#home"
   get "about" => "static_pages#about"
   get "help" => "static_pages#help"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  resources :users
 end
